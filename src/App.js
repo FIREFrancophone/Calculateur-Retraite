069d7a6d2b31c0e94fc93ecf6b187abaa70c7c34
FIREFrancophone <firefrancophone@gmail.com>
2020-10-29 12:26:08 -0500

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import { Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Grid';
 
-const App = () => (
-  <Button color="primary">Hello World</Button>
-)
+const useStyles = makeStyles( theme => ({
+  root: { flexGrow: 1 },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>Control-Thingie</Grid>
+        <Grid item xs={9}>Display-Thingie</Grid>
+      </Grid>
+    </div>
+  )
+}
 
 export default App;
